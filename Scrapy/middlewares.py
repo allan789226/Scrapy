e6dcf4331d9e83419dd6ad5f6c26d0c4776b34ff
allan789226 <illaholo15@gmail.com>
2024-06-21 11:09:03 +0300

    Add files via upload

---
+++ Scrapy/middlewares.py
@@ -0,0 +1,37 @@
+# Define here the middlewares for your spider
+    # See documentation in:
+    # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+    
+    # Disable cookies (enabled by default)
+    # COOKIES_ENABLED = False
+    
+    # Enable or disable downloader middlewares
+    # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+    # DOWNLOADER_MIDDLEWARES = {
+    #     'Scrapy.middlewares.ScrapyDownloaderMiddleware': 543,
+    # }
+    
+    # Enable or disable extensions
+    # See https://docs.scrapy.org/en/latest/topics/extensions.html
+    # EXTENSIONS = {
+    #     'scrapy.extensions.telnet.TelnetConsole': None,
+    # }
+    
+    # Configure item pipelines
+    # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+    # ITEM_PIPELINES = {
+    #     'Scrapy.pipelines.ScrapyPipeline': 300,
+    # }
+    
+    # Enable and configure the AutoThrottle extension (disabled by default)
+    # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+    # AUTOTHROTTLE_ENABLED = True
+    # The initial download delay
+    # AUTOTHROTTLE_START_DELAY = 5
+    # The maximum download delay to be set in case of high latencies
+    # AUTOTHROTTLE_MAX_DELAY = 60
+    # The average number of requests Scrapy should be sending in parallel to
+    # each remote server
+    # AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+    # Enable showing throttling stats for every response received:
+    # AUTOTHROTTLE_DEBUG = False
---
+++ Scrapy/spiders/quotes_spider.py
@@ -0,0 +1,20 @@
+import scrapy
+    from ..items import QuoteItem
+    
+    class QuotesSpider(scrapy.Spider):
+        name = 'quotes'
+        start_urls = [
+            'http://quotes.toscrape.com/'
+        ]
+    
+        def parse(self, response):
+            for quote in response.css('div.quote'):
+                item = QuoteItem()
+                item['text'] = quote.css('span.text::text').get()
+                item['author'] = quote.css('span small::text').get()
+                item['tags'] = quote.css('div.tags a.tag::text').getall()
+                yield item
+    
+            next_page = response.css('li.next a::attr(href)').get()
+            if next_page is not None:
+                yield response.follow(next_page, self.parse)
